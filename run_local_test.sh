42611d7cc7d21789b4bd9750c04bd8c578519f0a
0xMartin <martin.krcma1@gmail.com>
2024-09-17 00:20:25 +0200

    Test cnfg link changed

--- run_local_test.sh
+++ run_local_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./gradlew build
-java -jar NATT.jar -nc http://natt-config.wz.cz:8080/tasks/test-config-1.yaml
+java -jar NATT.jar -nc https://gitlab.com/ap5vs/test-config/email-sender-config/-/raw/main/test-config-1.yaml?ref_type=heads
