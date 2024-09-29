e6baf568060e130038452ad13d1e5c1fd3487483
d_polisensky <d_polisensky@utb.cz>
2024-09-29 14:53:35 +0200

    .

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -7,7 +7,7 @@ public class App {
         
         try {
             EmailSender sender = new EmailSender("smtp.utb.cz", 25);
-            sender.send("you@utb.cz", "you@utb.cz", "Email from Java", "Funguje to?\nSnad...");
+            sender.send("d_polisensky@utb.cz", "d_polisensky@utb.cz", "Email from Java", "Funguje to?\nSnad...");
             sender.close();
         } catch (Exception e) {
             e.printStackTrace();
--- app/src/main/java/utb/fai/EmailSender.java
+++ app/src/main/java/utb/fai/EmailSender.java
@@ -9,7 +9,11 @@ public class EmailSender {
      * during opening,
      * the exception is not handled in the constructor.
      */
-    public EmailSender(String host, int port) throws UnknownHostException, IOException {
+    private final Socket socket;
+
+
+    public EmailSender(String host, int port) throws  IOException {
+        this.socket = new Socket(host, port);
 
     }
 
@@ -20,13 +24,37 @@ public class EmailSender {
      * handled by this method.
      */
     public void send(String from, String to, String subject, String text) throws IOException {
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+        out.println("HELO utb.cz");
+        in.readLine();
+        out.println("MAIL FROM: " + from);
+        in.readLine();
+        out.println("RCPT TO: " + to);
+        in.readLine();
+        out.println("DATA");
+        in.readLine();
+        out.println("Subject: " + subject);
+        out.println(text);
+        out.println(".");
+        in.readLine();
     }
 
     /*
      * Sends QUIT and closes the socket
      */
     public void close() {
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            out.println("QUIT");
+            in.readLine();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
